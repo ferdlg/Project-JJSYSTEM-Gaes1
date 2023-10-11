a7e2cb82fcd8b5f969484af7a8e41660defb0a06
ferdlg <ferdlgpar@gmail.com>
2023-10-11 17:54:20 -0500

    Modelo de Administrador

---
+++ 05-Trimestre 6/jjSystem-project-java/src/main/java/com/api/jjSystem/models/Administrador.java
@@ -0,0 +1,40 @@
+package com.api.jjSystem.models;
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "administrador")
+public class Administrador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idAdministrador")
+    private Integer id;
+    private BigInteger numeroDocumento;
+
+    public Administrador() {
+    }
+    public Administrador(Integer id, BigInteger numeroDocumento) {
+        this.id = id;
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public BigInteger getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(BigInteger numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+}
