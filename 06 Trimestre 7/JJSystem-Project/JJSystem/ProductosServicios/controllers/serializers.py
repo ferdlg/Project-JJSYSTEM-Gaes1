e51f5a9f1a1c8d3dc2713bffe75f87af8904b429
ferdlg <ferdlgpar@gmail.com>
2024-01-29 23:45:10 -0500

    controllers, serializadores y modelos

---
+++ 06 Trimestre 7/JJSystem-Project/JJSystem/ProductosServicios/controllers/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from ..models import *
+
+#creamos una clase serializers para cada tabla
+class RolesSerializers(serializers.ModelSerializer):
+    #incluimos la clase Meta, para que le de instrucciones al serializador
+    class Meta:
+    #le indicamos el modelo que debe bucar
+        model = Roles
+    #le especificamos los campos que debe serializar
+        fields = '__all__'
+
+'''
+Nota: en tablas en las que debemos traer opciones,
+se deben especificar uno por uno los campos a serializar
+'''
+
+class UsuariosSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Usuarios
+        fields = '__all__'
+
+
+class ClientesSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Clientes
+        fields = '__all__'
+
+'''
+- categorias productos
+- categorias servicios
+- productos
+- servicios
+- proveedor producto 
+
+'''
---
+++ 06 Trimestre 7/JJSystem-Project/JJSystem/ServicioTecnico/controllers/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from ..models import *
+
+'''
+Serializadores tablas:
+- Tecnicos
+- Citas
+- Cotizaciones
+- Cronogramas
+- Actividades
+- detalles de actividades
+- disponibilidad cronogramas
+- estados cotizaciones
+'''
