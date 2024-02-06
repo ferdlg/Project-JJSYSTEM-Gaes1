62b90e97753a5c43f2d182c3e5307c0b4d4f1328
afcastro349 <113197066+afcastro349@users.noreply.github.com>
2024-02-06 13:39:26 -0500

    pruebas crud productos y servicios en funcionamiento

--- 06 Trimestre 7/prueba/jjsystemPrueba/ProductosServicios/models.py
+++ 06 Trimestre 7/prueba/jjsystemPrueba/ProductosServicios/models.py
@@ -393,8 +393,10 @@ class Servicios(models.Model):
     idcategoriaservicio = models.ForeignKey(Categoriasservicios, models.DO_NOTHING, db_column='idCategoriaServicio', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
-        managed = False
+        
         db_table = 'servicios'
+    
+    
 
 
 class Tecnicos(models.Model):
--- 06 Trimestre 7/prueba/jjsystemPrueba/jjsystemPrueba/settings.py
+++ 06 Trimestre 7/prueba/jjsystemPrueba/jjsystemPrueba/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'jjsystem_db',
         'USER': 'root',
-        'PASSWORD': '1021662854',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306'
     }
