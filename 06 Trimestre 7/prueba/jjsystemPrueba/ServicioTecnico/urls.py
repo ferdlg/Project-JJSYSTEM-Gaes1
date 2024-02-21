6e6ab82a4cdc8f7017e105facd9d540e09b2964d
ferdlg <ferdlgpar@gmail.com>
2024-02-21 12:12:05 -0500

    front citas

--- 06 Trimestre 7/prueba/jjsystemPrueba/ServicioTecnico/urls.py
+++ 06 Trimestre 7/prueba/jjsystemPrueba/ServicioTecnico/urls.py
@@ -9,7 +9,7 @@ from .controllers.detallesactividades import detallesactividadesCRUD
 from .controllers.disponibilidadcronograma import disponibilidadcronogramaCRUD
 from .controllers.estadocotizaciones import estadocotizacionesCRUD
 from .controllers.tecnicos import tecnicosCRUD
-from .views import index
+from .views import index , citaAnalisis , citaInstalacion , citaMantenimiento
 
 router = DefaultRouter()
 router.register(r'actividades_cronograma', actividadesCrogTecCRUD)
@@ -21,10 +21,11 @@ router.register(r'disponibilidad_cronograma', disponibilidadcronogramaCRUD)
 router.register(r'estado_cotizaciones',estadocotizacionesCRUD)
 router.register(r'tecnicos', tecnicosCRUD)
 
-urlpatterns = {
-    path('index/', index , name='index')
-}
 
 urlpatterns=[
     path('',include(router.urls)),
+    path('index/', index , name='index'),
+    path('cita_analisis', citaAnalisis, name='cita_analisis'),
+    path('cita_instalacion', citaInstalacion, name='cita_instalacion'),
+    path('cita_mantenimiento', citaMantenimiento, name='cita_mantenimiento'),
 ]
--- 06 Trimestre 7/prueba/jjsystemPrueba/ServicioTecnico/views.py
+++ 06 Trimestre 7/prueba/jjsystemPrueba/ServicioTecnico/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index (request):
     return render(request, 'index.html')
+
+def citaAnalisis(request):
+    return render(request, 'citaAnalisis.html')
+
+def citaInstalacion(request):
+    return render(request, 'citaInstalacion.html')
+
+def citaMantenimiento(request):
+    return render(request, 'citaMantenimiento.html')
