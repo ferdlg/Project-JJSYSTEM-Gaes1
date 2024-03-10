c5f1fef15fa0d2d17e356507c2d8fc19eae7623d
ferdlg <ferdlgpar@gmail.com>
2024-03-10 15:23:39 -0500

    organizacion del codigo en views

--- 06 Trimestre 7/prueba/jjsystemPrueba/ProductosServicios/controllers/productos.py
+++ 06 Trimestre 7/prueba/jjsystemPrueba/ProductosServicios/controllers/productos.py
@@ -1,6 +1,9 @@
+from django.shortcuts import redirect, render
 from rest_framework import viewsets
 from ..models import *
 from .serializers import ProductosSerializer
+from django.core.paginator import Paginator , EmptyPage , PageNotAnInteger
+
 
 '''
     Importamos la biblioteca viewsets 
@@ -14,3 +17,90 @@ class productosCRUD(viewsets.ModelViewSet):
     queryset = Productos.objects.all()
     # usamos serializer class para poder ver los objetos que se traen 
     serializer_class = ProductosSerializer
+
+# Productos en dashboard admin
+def home_productos(request):
+    # Obtener todos los productos
+    productos_list = Productos.objects.all()
+
+    # Configurar la paginación
+    paginator = Paginator(productos_list, 5)  # Mostrar 10 productos por página
+    page_number = request.GET.get('page')      # Obtener el número de página solicitado
+    try:
+        productos = paginator.page(page_number)
+    except PageNotAnInteger:
+        productos = paginator.page(1)
+    except EmptyPage:
+        productos = paginator.page(paginator.num_pages)
+
+    return render(request, "crudAdmin/IndexProductos.html", {"productos": productos})
+
+# crud de productos
+def createProductoView(request):
+    if request.method == 'POST':
+        nombreproducto = request.POST.get('nombre')
+        descripcionproducto = request.POST.get('descripcion')
+        precioproducto = request.POST.get('precio')
+        cantidad = request.POST.get('cantidad')
+        idcategoriaproducto = request.POST.get('categoria')
+        idproveedorproducto = request.POST.get('proveedor')
+            
+        # Obtener la instancia de categorias
+        categoria = Categoriasproductos.objects.get(idcategoriaproducto=idcategoriaproducto)
+
+        # Obtener la instancia de proveedor
+        proveedor = Proveedoresproductos.objects.get(idproveedorproducto=idproveedorproducto)
+
+        # Crear la instancia de producto
+        producto = Productos.objects.create(
+                nombreproducto = nombreproducto,
+                descripcionproducto = descripcionproducto,
+                precioproducto = precioproducto,
+                cantidad = cantidad,
+                idcategoriaproducto = categoria,
+                idproveedorproducto = proveedor
+            )
+        return redirect('homeProductos')
+
+    categorias = Categoriasproductos.objects.all()
+    proveedores = Proveedoresproductos.objects.all()
+    return render(request, "crudAdmin/CreateProducto.html", {"categorias": categorias, "proveedores": proveedores})
+
+def editarProducto(request, idProducto):
+    producto = Productos.objects.get(idproducto=idProducto)
+    categorias = Categoriasproductos.objects.all()
+    proveedores = Proveedoresproductos.objects.all()
+
+    if request.method == 'POST':
+        # Obtener los datos de la petición
+        nombreproducto = request.POST.get('nombre')
+        descripcionproducto = request.POST.get('descripcion')
+        precioproducto = request.POST.get('precio')
+        cantidad = request.POST.get('cantidad')
+        idcategoriaproducto = request.POST.get('categoria')
+        idproveedorproducto = request.POST.get('proveedor')
+
+        # Obtener la instancia de categorias
+        categoria = Categoriasproductos.objects.get(idcategoriaproducto=idcategoriaproducto)
+
+        # Obtener la instancia de proveedor
+        proveedor = Proveedoresproductos.objects.get(idproveedorproducto=idproveedorproducto)
+
+        # Actualizar los campos del objeto envio
+        producto.nombreproducto = nombreproducto
+        producto.descripcionproducto = descripcionproducto
+        producto.precioproducto = precioproducto
+        producto.cantidad = cantidad
+        producto.idcategoriaproducto = categoria
+        producto.idproveedorproducto = proveedor
+        producto.save()
+
+        return redirect('homeProductos')
+
+    return render(request, "crudAdmin/EditarProducto.html", {"producto": producto, "categorias": categorias, "proveedores": proveedores})
+
+def eliminarProducto(request, idProducto):
+    producto = Productos.objects.get(idproducto = idProducto)
+    producto.delete()
+
+    return redirect('homeProductos')
--- 06 Trimestre 7/prueba/jjsystemPrueba/ProductosServicios/urls.py
+++ 06 Trimestre 7/prueba/jjsystemPrueba/ProductosServicios/urls.py
@@ -2,15 +2,15 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
-from .controllers.categoriasproductos import categoriaproductosCRUD
-from .controllers.categoriasservicios import categoriaserviciosCRUD
+from .controllers.categoriasproductos import categoriaproductosCRUD , home_categoriaProductos , createCategoriaProductoView , editarCategoriaProductoView, eliminarCategoriaProductoView
+from .controllers.categoriasservicios import categoriaserviciosCRUD , home_categoriaServicios, createCategoriaServicioView, editarCategoriaServicioView, eliminarCategoriaServicioView
 from .controllers.clientes import clientesCRUD
 from .controllers.permisos import permisosCRUD
-from .controllers.productos import productosCRUD
+from .controllers.productos import productosCRUD , home_productos , createProductoView, editarProducto, eliminarProducto
 from .controllers.proveedoresproductos import proveedoresCRUD
 from .controllers.rol_has_permisos import rol_has_permisosCRUD
 from .controllers.roles import rolesCRUD
-from .controllers.servicios import serviciosCRUD
+from .controllers.servicios import serviciosCRUD, servicios, servicios_landing, home_servicios, createServiciosView, editarServicio, eliminarServicio
 
 router = DefaultRouter()
 router.register(r'categoriasproductos', categoriaproductosCRUD)
@@ -25,28 +25,28 @@ router.register(r'servicios', serviciosCRUD)
 
 urlpatterns=[
     path('', views.home, name='home'),
-    path('productos', views.home_productos, name='homeProductos'),
-    path('serviciosLanding/<int:categoria>/', views.servicios_landing, name='serviciosLanding'),
+    path('productos',home_productos, name='homeProductos'),
+    path('serviciosLanding/<int:categoria>/',servicios_landing, name='serviciosLanding'),
     path('verProducto/<id>/', views.producto, name='verProducto'),
     path('api/', include(router.urls)),
 
-    path('createProducto/', views.createProductoView, name='createProducto'),
-    path('editarProducto/<idProducto>', views.editarProducto, name='editarProducto'),
-    path('eliminarProducto/<idProducto>', views.eliminarProducto, name='eliminarProducto'),
+    path('createProducto/',createProductoView, name='createProducto'),
+    path('editarProducto/<idProducto>',editarProducto, name='editarProducto'),
+    path('eliminarProducto/<idProducto>',eliminarProducto, name='eliminarProducto'),
 
-    path('categoriaProducto/', views.home_categoriaProductos, name='categoriaProductos'),
-    path('crearCategoriaProducto/', views.createCategoriaProductoView, name = 'createCategoriaProducto'),
-    path('editarCategoriaProducto/<idCategoriaProducto>', views.editarCategoriaProductoView, name='editarCategoriaProducto'),
-    path('eliminarCategoriaProducto/<idCategoriaProducto>', views.eliminarCategoriaProductoView, name = 'eliminarCategoriaProducto'),
+    path('categoriaProducto/', home_categoriaProductos, name='categoriaProductos'),
+    path('crearCategoriaProducto/', createCategoriaProductoView, name = 'createCategoriaProducto'),
+    path('editarCategoriaProducto/<idCategoriaProducto>', editarCategoriaProductoView, name='editarCategoriaProducto'),
+    path('eliminarCategoriaProducto/<idCategoriaProducto>', eliminarCategoriaProductoView, name = 'eliminarCategoriaProducto'),
 
-    path('servicios/', views.home_servicios, name='homeServicios'),
-    path('createServicio/', views.createServiciosView, name='createServicio'),
-    path('editarServicio/<idServicio>', views.editarServicio, name='editarServicio'),
-    path('eliminarServicio/<idServicio>', views.eliminarServicio, name='eliminarServicio'),
+    path('servicios/', home_servicios, name='homeServicios'),
+    path('createServicio/', createServiciosView, name='createServicio'),
+    path('editarServicio/<idServicio>', editarServicio, name='editarServicio'),
+    path('eliminarServicio/<idServicio>', eliminarServicio, name='eliminarServicio'),
 
-    path('categoriaServicio/', views.home_categoriaServicios, name = 'categoriaServicios'),
-    path('createCategoriaServicio/', views.createCategoriaServicioView, name="createCategoriaServicio"),
-    path('editarCategoriaServicio/<idCategoriaServicio>', views.editarCategoriaServicioView, name='editarCategoriaServicio'),
-    path('eliminarCategoriaServicio/<idCategoriaServicio>', views.eliminarCategoriaServicioView, name = 'eliminarCategoriaServicio')
+    path('categoriaServicio/', home_categoriaServicios, name = 'categoriaServicios'),
+    path('createCategoriaServicio/', createCategoriaServicioView, name="createCategoriaServicio"),
+    path('editarCategoriaServicio/<idCategoriaServicio>', editarCategoriaServicioView, name='editarCategoriaServicio'),
+    path('eliminarCategoriaServicio/<idCategoriaServicio>', eliminarCategoriaServicioView, name = 'eliminarCategoriaServicio')
 
 ]
